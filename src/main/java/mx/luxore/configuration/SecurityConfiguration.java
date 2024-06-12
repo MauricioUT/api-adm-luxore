de1eecbac3e70bcae42e0e0022d416c19420fd6e
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-06-12 15:36:20 -0600

    add new security filter

--- src/main/java/mx/luxore/configuration/SecurityConfiguration.java
+++ src/main/java/mx/luxore/configuration/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package mx.luxore.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,19 +14,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Configuration
-@RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         List<String> allowDomains = new ArrayList<>();
         allowDomains.add("http://localhost:4200");
-        allowDomains.add("http://localhost:8080");
-        allowDomains.add("https://stellar-psyche-312620.uc.r.appspot.com");
-        allowDomains.add("https://soonercitasback.uc.r.appspot.com");
-        allowDomains.add("https://citas-arranca-patio.uc.r.appspot.com");
-
-
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
@@ -36,10 +35,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return source;
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http.authorizeRequests().antMatchers("/_ah/start").permitAll();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+
     }
 }
 
